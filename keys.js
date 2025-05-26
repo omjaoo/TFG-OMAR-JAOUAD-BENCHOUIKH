cff5906cef64fec9960d5a778888e43fc276908a
Omar Jaouad <jaouadomar@hotmail.es>
2025-05-26 11:28:41 +0200

    [bbdd atlas desplegada]

--- keys.js
+++ keys.js
@@ -1,7 +1,8 @@
 // ruta de la bbdd
 module.exports = {
     mongodb: {
-      URI: 'mongodb://localhost:27017/TFG_Omar_Jaouad_Benchouikh'
+      /* URI: 'mongodb://localhost:27017/TFG_Omar_Jaouad_Benchouikh'   */
+      URI: 'mongodb+srv://omar:CZl5U3I0dMJnz6Ob@cluster0.7qnys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'  
     },
-    api_key_casas: 'JgLgIugtPRry2jlEgx7if1N0gZpGxsqjxx83wZAR'
+    api_key_casas: 'zCfJPQ2xKuy0f11Qn7eYOmoR3QXvZsbWfYUQ9aAr'
 };
