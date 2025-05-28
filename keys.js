51f7aa2b8ed3c7a4c41333c7b8d6008da16d4d08
Omar Jaouad <jaouadomar@hotmail.es>
2025-05-28 12:09:55 +0200

    [omjaoo] cambios generales

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ module.exports = {
        /* URI: 'mongodb://localhost:27017/TFG_Omar_Jaouad_Benchouikh'  */  
       URI: 'mongodb+srv://omar:CZl5U3I0dMJnz6Ob@cluster0.7qnys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
     },
-    api_key_casas: '8G2SZoPp5w6LY2fFpCioLg02uYdEDMqu0ot7PA1X'
+    api_key_casas: 'HZfmwUTPQ4Df6L8WH4Yq18Zxv64XglS7MOfEltpM'
 };
